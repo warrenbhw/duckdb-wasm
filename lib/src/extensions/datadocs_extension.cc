efffc5afc12398a9c7eedda53528b4cca66bd413
handstuyennn <tuyennn@ihands.com.vn>
2023-07-19 11:02:45 +0700

    update extension datadocs

--- lib/src/extensions/datadocs_extension.cc
+++ lib/src/extensions/datadocs_extension.cc
@@ -1,5 +1,5 @@
 #include "duckdb/web/extensions/datadocs_extension.h"
 
-#include "datadocs-extension.hpp"
+#include "datadocs_extension.hpp"
 
-extern "C" void duckdb_web_datadocs_init(duckdb::DuckDB* db) { db->LoadExtension<duckdb::DataDocsExtension>(); }
+extern "C" void duckdb_web_datadocs_init(duckdb::DuckDB* db) { db->LoadExtension<duckdb::DatadocsExtension>(); }
